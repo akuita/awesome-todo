4c285d32bcc19157c208b62031eab54eaad9ba9d
Carmina (Jitera) <developers@jitera.com>
2024-01-09 22:26:49 +0000

    feat[service]: implement Check email availability

--- app/controllers/api/users_registrations_controller.rb
+++ app/controllers/api/users_registrations_controller.rb
@@ -1,4 +1,20 @@
 class Api::UsersRegistrationsController < Api::BaseController
+  before_action :validate_email_format, only: [:check_email_availability]
+
+  def check_email_availability
+    if User.exists?(email: params[:email])
+      render json: { message: I18n.t('controller.users_registrations.email_in_use') }, status: :ok
+    else
+      render json: { message: I18n.t('controller.users_registrations.email_available') }, status: :ok
+    end
+  end
+
+  private
+
+  def validate_email_format
+    render json: { message: I18n.t('controller.users_registrations.invalid_email') }, status: :unprocessable_entity unless params[:email].match?(URI::MailTo::EMAIL_REGEXP)
+  end
+
   def create
     @user = User.new(create_params)
     if @user.save
