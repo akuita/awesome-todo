7ec0dc406133ba2413fb86dbdd6a4c6aaacfd200
Dulcea (Jitera) <developers@jitera.com>
2024-01-12 00:11:41 +0000

    feat[business-logic]: implement 'Email confirmation' Changes * '/app/controllers/api/users_verify_confirmation_token_controller.rb': + Create an action `create` within the `UsersVerifyConfirmationTokenController` to handle the email confirmation process..+ The action should accept a "token" parameter from the request..+ Use a service object to encapsulate the business logic for verifyin...

--- app/controllers/api/users_verify_confirmation_token_controller.rb
+++ app/controllers/api/users_verify_confirmation_token_controller.rb
@@ -1,20 +1,38 @@
+
 class Api::UsersVerifyConfirmationTokenController < Api::BaseController
+  include OauthTokensConcern
+
   def create
     client = Doorkeeper::Application.find_by(uid: params[:client_id], secret: params[:client_secret])
-    raise  Exceptions::AuthenticationError if client.blank?
+    raise Exceptions::AuthenticationError if client.blank?
 
-    resource = User.find_by(confirmation_token: params.dig(:confirmation_token))
-    if resource.blank? || params.dig(:confirmation_token).blank?
-      render error_message: I18n.t('email_login.reset_password.invalid_token'),
+    token = params[:token]
+    email_confirmation = EmailConfirmation.find_by(token: token)
+
+    if email_confirmation.blank? || token.blank?
+      render json: { error_message: I18n.t('email_confirmation.invalid_token') },
              status: :unprocessable_entity and return
     end
 
-    if (resource.confirmation_sent_at + User.confirm_within) < Time.now.utc
-      resource.resend_confirmation_instructions
-      render json: { error_message: I18n.t('email_login.reset_password.expired') }, status: :unprocessable_entity
-    else
-      resource.confirm
-      custom_token_initialize_values(resource, client)
+    if email_confirmation.expires_at < Time.now.utc
+      render json: { error_message: I18n.t('email_confirmation.expired') },
+             status: :unprocessable_entity and return
     end
+
+    user = email_confirmation.user
+    if user.email_confirmed
+      render json: { error_message: I18n.t('email_confirmation.already_confirmed') },
+             status: :unprocessable_entity and return
+    end
+
+    user.email_confirmed = true
+    email_confirmation.confirmed = true
+    ActiveRecord::Base.transaction do
+      user.save!
+      email_confirmation.save!
+    end
+
+    sign_in(user)
+    render json: { message: I18n.t('email_confirmation.success') }, status: :ok
   end
 end
