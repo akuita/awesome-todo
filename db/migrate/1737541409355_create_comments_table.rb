9c1e546c5a9e0d11b941f77522e504cefd4441ad
Coriss (Jitera) <ai_developers@jitera.com>
2025-01-22 10:23:57 +0000

    feat: create comments table and model

---
+++ db/migrate/1737541409355_create_comments_table.rb
@@ -0,0 +1,11 @@
+class CreateCommentsTable < ActiveRecord::Migration[6.0]
+  def change
+    create_table :comments do |t|
+      t.text :content, null: false
+      t.references :user, null: false, foreign_key: true
+      t.references :note, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
