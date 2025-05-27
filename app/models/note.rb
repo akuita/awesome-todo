3a1ffdbbf12cb5657f8252582f0a5a9d423e4a9b
MQuy <quy.truong@jitera.com>
2025-05-27 21:44:56 +0200

    feat: add label

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,6 +3,7 @@ class Note < ApplicationRecord
 
   validates :title, length: { in: 0..255 }, if: :title?
   validates :tags, length: { in: 0..255 }, if: :title?
+  validates :label, length: { in: 0..255 }, if: :title?
   validates :description, length: { in: 0..65_535 }, if: :description?
 
   # end for validations
