d67dfbc57632c5b86bb69a5ab459249cfb9d7003
MQuy <quy.truong@jitera.com>
2025-05-27 22:48:10 +0200

    feat: add comments

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,7 +3,6 @@ class Note < ApplicationRecord
 
   validates :title, length: { in: 0..255 }, if: :title?
   validates :tags, length: { in: 0..255 }, if: :tags?
-  validates :comments, length: { in: 0..255 }, if: :comments?
   validates :description, length: { in: 0..65_535 }, if: :description?
 
   # end for validations
