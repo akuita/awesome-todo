f6d00a6d2b0ac03097d2516118a9fc55e81e5c2b
MQuy <quy.truong@jitera.com>
2025-05-27 22:42:33 +0200

    feat: add tags

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,7 +2,8 @@ class Note < ApplicationRecord
   # validations
 
   validates :title, length: { in: 0..255 }, if: :title?
-
+  validates :tags, length: { in: 0..255 }, if: :tags?
+  validates :comments, length: { in: 0..255 }, if: :comments?
   validates :description, length: { in: 0..65_535 }, if: :description?
 
   # end for validations
